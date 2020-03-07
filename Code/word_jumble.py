21e3f97fe984a9f54601aad7c2e6044657a90cfd
MondaleFelix <mondalefelix@yahoo.com>
2020-03-07 07:33:48 -0800

    added word jumble

--- Code/word_jumble.py
+++ Code/word_jumble.py
@@ -1,57 +1,40 @@
 import math
-# Create a set of all permutations
-# 	Recursively Sort the set from ascending order
-# Create another set of dictionary words
-# Get difference between sets
 
-  
+# Create a histogram of counts of letters in dictionary
+# Create a histogram of counts of letters in given word
 
-# Function to print permutations of string 
-# This function takes three parameters: 
-# 1. String 
-# 2. Starting index of the string 
-# 3. Ending index of the string. 
-
-
-#  [123]
-# [321, 312, 132, 123, 213, 231]
-
-# [123, 213, 231, ]
-
-# [1234]
-
-# [4321, 4312, 4132, 1432, 1423, 1243, 2143, ]
-
-# Total amount of permutations is n!
-
-
-def get_all_permutations(str):
-	total_amount = get_total_permutations(str)
-
-	perumtaions = []
-
-	if len(perumtaions) is total_amount:
-		return perumtaions
-
-	if len(perumtaions) is not total_amount:
-
-
-# [1,2,3]
-
-
-def test(str):
-	working_list = list(str)
-
-	for i in working_list:
-		for numbner 
-
-
-def swap_positions(list, pos1, pos2):
-	for i in range(len(list) -1):
-	pass
+# Return the word that is found in dictionary where histogram matches
 
 
 
-def get_total_permutations(str):
-	count = len(str)
-	return math.factorial(count)
+with open("/usr/share/dict/words") as f:
+    lines = f.read().lower()
+    dictionary = lines.split('\n')
+
+
+
+def create_histogram(str):
+	d = {}
+	for letter in str.lower():
+		if letter in d:
+			d[letter] += 1
+		else:
+			d[letter] = 1
+
+	return d
+
+
+def get_words_list(str):
+	possible_words = []
+	d = create_histogram(str)
+	for word in dictionary:
+		word_hist = create_histogram(word)
+		if d == word_hist:
+			possible_words.append(word)
+
+	return possible_words
+
+print(get_words_list("LAISA"))
+print(get_words_list("LAURR"))
+print(get_words_list("BUREEK"))
+print(get_words_list("PROUOT"))
